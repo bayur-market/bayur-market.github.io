c2f19918b5d65d1e03f1215ea5f138b5f8e61a6b
ahmadmcer <ahmadmcer@gmail.com>
2023-06-21 20:11:06 +0700

    fix: showing join total

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+const joinTotal = document.querySelector('#joinTotal');
+
+fetch('https://sheetdb.io/api/v1/4adp0n2gipwg2/count')
+  .then((response) => response.json())
+  .then((data) => {
+    joinTotal.innerHTML = data.rows;
+  });
+
 function formSubmit(data) {
   fetch('https://sheetdb.io/api/v1/4adp0n2gipwg2', {
     method: 'POST',
